c3654955f3ca50806aa9cf370493df086b3ed68e
Marcus Mellor <marcus@mellor.com>
2019-03-14 17:51:53 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown (num_secs)
+  while num_secs>0 
+  
+    puts "#{num_secs} SECOND(S)!"
+    num_secs-=1
+  end 
+  phrase="HAPPY NEW YEAR!"
+#  puts phrase
+  phrase
+end
+
+def countdown_with_sleep( num_secs)
+  while num_secs>0 
+  
+    puts "#{num_secs} SECOND(S)!"
+    num_secs-=1
+    sleep(1)
+  end 
+  phrase="HAPPY NEW YEAR!"
+#  puts phrase
+  phrase
+  
 end
